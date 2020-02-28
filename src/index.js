5fd33932790ea50b377a055b7fa4e7f3febaa2e0
Asiliia <51196040+Asiliia@users.noreply.github.com>
2020-02-28 03:04:07 +0300

    feat: add task

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 
 exports.min = function min (array) {
-  return 0;
+  if(!array){
+    return 0;
+  }
+  return array.length > 0 ? Math.min(...array) : 0;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if(!array){
+    return 0;
+  }
+  return array.length > 0 ? Math.max(...array) : 0;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if(!array){
+    return 0;
+  }
+  return array.length > 0 ? (array.reduce((sum, item) => sum + item) / array.length).toFixed(3) : 0;
 }
